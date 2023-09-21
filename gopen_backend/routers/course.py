d70429f6746849b433dc31c0e6cb2ebb436912d3
miyago9267 <miyagomizugen@gmail.com>
2023-09-22 02:57:58 +0800

    feat: init course router

--- gopen_backend/routers/course.py
+++ gopen_backend/routers/course.py
@@ -1,4 +1,16 @@
 from fastapi import APIRouter
+from controllers import course
 
 router = APIRouter()
 
+@router.get('/')
+async def get_all_courses(search: str = None):
+    return course.get_all_courses(search=search)
+
+@router.get('/all')
+async def get_all_courses():
+    return course.get_all_courses()
+
+@router.get('/{teacher_id}/{course_id}')
+async def get_all_courses(teacher_id: int, course_id: int):
+    return {'course': {}}
