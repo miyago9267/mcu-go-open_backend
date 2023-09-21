792b524325bc8f2189d8c5f707959b98114f86fc
miyago9267 <miyagomizugen@gmail.com>
2023-09-22 02:57:41 +0800

    feat: init login/out router

--- gopen_backend/routers/auth.py
+++ gopen_backend/routers/auth.py
@@ -1,3 +1,11 @@
 from fastapi import APIRouter
 
 router = APIRouter()
+
+@router.post('/login')
+async def login():
+    return {'login': True}
+
+@router.post('/logout')
+async def logout():
+    return {'logout': True}
